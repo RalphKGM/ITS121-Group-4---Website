5371ffc5c1572a732e0c58d8b9ad6e7c6996874d
Ralph Kevin G. Morales <rkgmorales@mymail.mapua.edu.ph>
2025-03-14 22:22:11 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,81 @@
+document.getElementById('productsMenu').addEventListener('click', function() {
+    const bottomNavbar = document.querySelector('.bottom-navbar');
+    const caretIcon = document.getElementById('caretIcon');
+    
+    bottomNavbar.classList.toggle('active'); 
+    
+    if (bottomNavbar.classList.contains('active')) {
+        caretIcon.classList.remove('fa-caret-down');
+        caretIcon.classList.add('fa-caret-up');
+    } else {
+        caretIcon.classList.remove('fa-caret-up');
+        caretIcon.classList.add('fa-caret-down');
+    }
+});
+
+/* 
+document.querySelectorAll('.bottom-navbar .nav-link').forEach(link => {
+    link.addEventListener('mouseover', function() {
+        const id = link.id + 'Image';
+        const image = document.querySelector(`#${id}`);
+        if (image) {
+            image.classList.add('show');
+        }
+    });
+
+    link.addEventListener('mouseout', function() {
+        const id = link.id + 'Image';
+        const image = document.querySelector(`#${id}`);
+        if (image) {
+            image.classList.remove('show');
+        }
+    });
+}); */
+
+// scroll navbar anim
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+//products menu navbar
+const productsMenu = document.getElementById('productsMenu');
+const bottomNavbar = document.querySelector('.bottom-navbar');
+const caretIcon = document.getElementById('caretIcon');
+
+let hoverTimeout;
+
+//show bottom navbar
+productsMenu.addEventListener('mouseenter', function () {
+    clearTimeout(hoverTimeout);
+    bottomNavbar.classList.add('active');
+    caretIcon.classList.remove('fa-caret-down');
+    caretIcon.classList.add('fa-caret-up');
+});
+
+//hide bottom navbar when no hover
+productsMenu.addEventListener('mouseleave', function () {
+    hoverTimeout = setTimeout(() => {
+        bottomNavbar.classList.remove('active');
+        caretIcon.classList.remove('fa-caret-up');
+        caretIcon.classList.add('fa-caret-down');
+    }, 300); // delay, change if u want it slow 
+});
+
+//bottom navbar stay while mouse hover
+bottomNavbar.addEventListener('mouseenter', function () {
+    clearTimeout(hoverTimeout);
+    bottomNavbar.classList.add('active');
+});
+
+bottomNavbar.addEventListener('mouseleave', function () {
+    hoverTimeout = setTimeout(() => {
+        bottomNavbar.classList.remove('active');
+        caretIcon.classList.remove('fa-caret-up');
+        caretIcon.classList.add('fa-caret-down');
+    }, 300); // delay, change if u want it slow 
+});
